fa82e549d240e73af82f53787d5c91c0cdb4401f
aryaantony92 <arya.antony@pimcore.com>
2023-04-24 09:43:34 +0200

    Fix tests

--- .github/ci/files/config/bundles.php
+++ .github/ci/files/config/bundles.php
@@ -1,6 +1,5 @@
 <?php
 
 return [
-    "Pimcore\\Bundle\\AdminBundle\\PimcoreAdminBundle" => ['all' => true],
     "Pimcore\\Bundle\\PersonalizationBundle\\PimcorePersonalizationBundle" => ['all' => true],
 ];
