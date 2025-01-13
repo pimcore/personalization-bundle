8b9c5caba3e1bd9f9804057faf05de2af13abbc6
Bernhard Rusch <brusch@users.noreply.github.com>
2025-01-13 15:47:07 +0100

    Update TargetGroupMultiselect.php

--- src/Pimcore/Model/DataObject/ClassDefinition/Data/TargetGroupMultiselect.php
+++ src/Pimcore/Model/DataObject/ClassDefinition/Data/TargetGroupMultiselect.php
@@ -48,8 +48,7 @@ class TargetGroupMultiselect extends Model\DataObject\ClassDefinition\Data\Multi
     public static function __set_state(array $data): static
     {
         $obj = parent::__set_state($data);
-        $options = $obj->getOptions();
-        if (\Pimcore::inAdmin() || empty($options)) {
+        if (\Pimcore::inAdmin()) {
             $obj->configureOptions();
         }
 
