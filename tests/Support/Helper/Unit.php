4ccbe945cb20c923a29d587823642cbf43639ee7
Alex Zamponi <562324+alexz707@users.noreply.github.com>
2025-04-02 17:17:48 +0200

    Remove redundant migration table creation code from Model and Unit classes

--- tests/Support/Helper/Unit.php
+++ tests/Support/Helper/Unit.php
@@ -30,19 +30,6 @@ class Unit extends \Codeception\Module
         /** @var Pimcore $pimcoreModule */
         $pimcoreModule = $this->getModule('\\' . Pimcore::class);
 
-        //create migrations table in order to allow installation - needed for SettingsStoreAware Installer
-
-        Db::get()->executeStatement('
-            create table migration_versions
-            (
-                version varchar(1024) not null
-                    primary key,
-                executed_at datetime null,
-                execution_time int null
-            )
-            collate=utf8_unicode_ci;
-            ');
-
         $installer = $pimcoreModule->getContainer()->get(Installer::class);
         $installer->install();
     }
