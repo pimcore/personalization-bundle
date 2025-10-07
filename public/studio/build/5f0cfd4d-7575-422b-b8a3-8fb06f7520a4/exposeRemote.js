08180575aa1501482cbc3a752e995b747ce32783
xIrusux <42359615+xIrusux@users.noreply.github.com>
2025-10-07 20:10:11 +0000

    Automatic frontend build

---
+++ public/studio/build/5f0cfd4d-7575-422b-b8a3-8fb06f7520a4/exposeRemote.js
@@ -0,0 +1,7 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      window.pluginRemotes.pimcore_personalization_bundle = "/bundles/pimcorepersonalization/studio/build/5f0cfd4d-7575-422b-b8a3-8fb06f7520a4/static/js/remoteEntry.js"
+    
